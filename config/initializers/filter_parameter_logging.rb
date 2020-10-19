4f85d71ce5852579d84f4136729db36a3028d9ca
Maciorex <maciej.zabski@netguru.pl>
2020-10-19 23:56:48 +0200

    set filter parameters logging

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :cvv, :card_number, :billing_zip_code, :card_expiration_date]
